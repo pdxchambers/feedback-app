11eaaa3476812dec580128647008c4b5eb3edf4e
Julien Chambers <pdxchambers@gmail.com>
2022-09-05 09:57:06 -0700

    final version with known bugs

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -17,28 +17,45 @@ export const FeedbackProvider = ({children}) => {
 
   //fetch feedback
   const fetchFeedback = async () =>{
-    const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+    const response = await fetch(`/feedback?_sort=id&_order=desc`)
     const data = await response.json()
     setFeedback(data)
     setIsLoading(false)
   }
 
   // Delete feedback
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if(window.confirm('Are you sure you want to delete?')){
+      await fetch(`/feedback/${id}`, {method: 'DELETE'})
       setFeedback(feedback.filter((item) => item.id !== id));
     }  
   }
 
   // Update feedback item
-  const updateFeedback = (id, updItem) => {
-    setFeedback(feedback.map((item) => item.id === id ? {...item, ...updItem} : item))
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem)
+    })
+    const data = await response.json()
+    setFeedback(feedback.map((item) => item.id === id ? {...item, ...data} : item))
   }
 
   // Add feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newFeedback),
+    })
+    
+    const data = await response.json()
+    setFeedback([data, ...feedback])
   }
 
   // Set item to be updated
