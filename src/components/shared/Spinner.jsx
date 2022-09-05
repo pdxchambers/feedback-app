5a603bfc0849bad2d338c61ba910a2b80b423021
Julien Chambers <pdxchambers@gmail.com>
2022-09-05 09:24:05 -0700

    fix inline spinner css

--- src/components/shared/Spinner.jsx
+++ src/components/shared/Spinner.jsx
@@ -4,7 +4,7 @@ function Spinner() {
   return <img 
     src={spinner} 
     alt="Loading..." 
-    style="{{width: '100px', margin: 'auto', display: 'block'}}" 
+    style={{width: '100px', margin: 'auto', display: 'block'}} 
   />
 }
 
